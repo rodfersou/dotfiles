2de6a6f1b499bd221394996e4e093a99e3bc0800
Rodrigo Ferreira de Souza <rodfersou@gmail.com>
2020-09-06 19:30:30 +0700

    Update rxvt color and font schema

--- common/zsh-custom/aliases.d/antlr.zsh
+++ common/zsh-custom/aliases.d/antlr.zsh
@@ -1,11 +1,11 @@
-if [ -f ~/.asdf/plugins/java/set-java-home.sh ]
-then
-  source ~/.asdf/plugins/java/set-java-home.sh
-elif [ -f ~/.asdf/plugins/java/set-java-home.zsh ]
-then
-  source ~/.asdf/plugins/java/set-java-home.zsh
-fi
+# if [ -f ~/.asdf/plugins/java/set-java-home.sh ]
+# then
+#   source ~/.asdf/plugins/java/set-java-home.sh
+# elif [ -f ~/.asdf/plugins/java/set-java-home.zsh ]
+# then
+#   source ~/.asdf/plugins/java/set-java-home.zsh
+# fi
 
-export CLASSPATH=".:$HOME/.lib/antlr-4.8-complete.jar:$CLASSPATH"
-alias antlr4="java -jar $HOME/.lib/antlr-4.8-complete.jar"
-alias grun='java org.antlr.v4.gui.TestRig'
+# export CLASSPATH=".:$HOME/.lib/antlr-4.8-complete.jar:$CLASSPATH"
+# alias antlr4="java -jar $HOME/.lib/antlr-4.8-complete.jar"
+# alias grun='java org.antlr.v4.gui.TestRig'
