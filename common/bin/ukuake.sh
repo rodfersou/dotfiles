ea3776fc2c476e42aabedf7af7dc76579ccd1fa8
Rodrigo Ferreira de Souza <rodfersou@gmail.com>
2020-09-20 14:06:41 +0700

    Fix ukuake config

--- common/bin/ukuake.sh
+++ common/bin/ukuake.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-urxvt -name "UKuake" -T "UKuake" -depth 32 -bg rgba:3f00/3f00/3f00/eeee +sb -pe default,kuake -kuake-hotkey F1 -e screen -RRaAD > /dev/null 2> /dev/null &
+urxvt -name "UKuake" -T "UKuake" -depth 32 -bg rgba:0000/0000/0000/dddd +sb -pe default,kuake -kuake-hotkey F1 -e screen -RRaAD > /dev/null 2> /dev/null &
