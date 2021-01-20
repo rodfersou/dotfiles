2ebf7bbe76872fcbd3eee3e4d11a6c849e03ca0d
Rodrigo Ferreira de Souza <rodfersou@gmail.com>
2021-01-20 23:22:46 +0700

    Update

--- common/bin/ukuake.sh
+++ common/bin/ukuake.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
 urxvt -name "UKuake" -T "UKuake" -depth 32 -bg rgba:0000/0000/0000/eeee +sb -pe default,kuake -kuake-hotkey F1 -e screen -RRaAD > /dev/null 2> /dev/null &
+urxvt -name "UKuake2" -T "UKuake2" -depth 32 -bg rgba:0000/0000/0000/eeee +sb -pe default,kuake -kuake-hotkey F2 > /dev/null 2> /dev/null &
