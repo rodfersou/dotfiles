1809578ca7972f9a3f718642f19178417e643849
Rodrigo Ferreira de Souza <rodfersou@gmail.com>
2024-02-05 09:59:57 +0800

    Nvim config

---
+++ common/config/nvim/lua/plugins/vim-visual-multi.lua
@@ -0,0 +1,14 @@
+return {
+  "mg979/vim-visual-multi",
+  lazy = false,
+  init = function()
+    vim.g.VM_theme = "iceblue"
+    vim.g.VM_maps = {
+      ["Find Under"] = "<C-d>",
+      ["Find Subword Under"] = "<C-d>",
+      ["Select Cursor Down"] = "<M-C-Down>",
+      ["Select Cursor Up"] = "<M-C-Up>",
+      ["Visual Cursors"] = "<C-d>",
+    }
+  end,
+}
