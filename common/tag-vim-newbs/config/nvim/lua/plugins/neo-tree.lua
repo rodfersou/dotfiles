7f6d4eac50a234275bafecc39f77891476fae112
Rodrigo Ferreira de Souza <rodfersou@gmail.com>
2024-04-03 18:17:43 +0800

    Fix neo-tree

--- common/tag-vim-newbs/config/nvim/lua/plugins/neo-tree.lua
+++ common/tag-vim-newbs/config/nvim/lua/plugins/neo-tree.lua
@@ -7,17 +7,30 @@ return {
 		"MunifTanjim/nui.nvim",
 	},
 	config = function()
+    require("neo-tree").setup({
+      event_handlers = {
+        {
+          event = "file_opened",
+          handler = function()
+            require("neo-tree.command").execute({
+              action = "close"
+            })
+          end
+        },
+      }
+    })
 		vim.keymap.set("n", "<F12>", ":Neotree toggle right<CR>", {})
 		vim.keymap.set("n", "<leader>bf", ":Neotree buffers reveal float<CR>", {})
-	end,
-  opts = {
-    event_handlers = {
-      {
-        event = "file_opened",
-        handler = function(file_path)
-          require("neo-tree.command").execute({ action = "close" })
+    vim.api.nvim_create_autocmd({"BufEnter", "BufWinEnter"}, {
+      pattern = {"*"},
+      callback = function()
+        if vim.fn.argc() == 0 then
+          require("neo-tree.command").execute({
+            toggle = true,
+            position = "right",
+          })
         end
-      },
-    },
-  },
+      end
+    })
+	end
 }
