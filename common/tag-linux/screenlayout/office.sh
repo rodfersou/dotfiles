3bf72fbef72ec598f063538d7481e9f3d6f82d17
Rodrigo Ferreira de Souza <rodfersou@gmail.com>
2024-10-02 21:05:03 +0800

    Update configs

--- common/tag-linux/screenlayout/office.sh
+++ common/tag-linux/screenlayout/office.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output HDMI1 --mode 1920x1080 --pos 0x0 --rotate normal --output VIRTUAL1 --off --output eDP1 --primary --mode 1366x768 --pos 1920x408 --rotate normal
+xrandr --output eDP-1 --mode 1920x1200 --pos 0x0 --rotate normal --output HDMI-1 --off --output DP-1 --mode 1920x1080 --pos 1920x262 --rotate normal --output DP-2 --off --output DP-3 --off --output DP-4 --off
